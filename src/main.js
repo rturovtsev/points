133e5e9c9f0263429690f098ca47c4d522d0b1f6
sugrob69 <sugrob.boid@gmail.com>
2016-06-29 17:53:34 +0300

    ref

--- src/main.js
+++ src/main.js
@@ -1,27 +1,11 @@
 import React, { Component } from 'react' //, PropTypes
 import { render } from 'react-dom'
+import Canvas from './components/canvas';
 
 export default class App extends Component {
-	componentDidMount() {
-		this.updateCanvas();
-	}
-
-	updateCanvas() {
-		const canvas = this.refs.canvas,
-			ctx = canvas.getContext('2d');
-
-		let width = 700,
-			height = 500;
-
-		canvas.width = width;
-		canvas.height = height;
-
-		ctx.strokeRect(0, 0, width, height);
-	}
-
 	render() {
 		return (
-			<canvas ref='canvas'></canvas>
+			<Canvas/>
 		);
 	}
 }
